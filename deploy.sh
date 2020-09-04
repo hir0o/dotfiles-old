d352ec0b817e7b9e2c045ff0f8fd1e05b344fb90
shibuyahiroyuki <mk.9.q.kyu@gmail.com>
2020-09-04 21:41:48 +0900

    add snippets

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,3 @@
-VSPATH="${HOME}/Library/Application Support/Code/User" && mkdir -p ${VSPATH}
-
-
 for f in .??*
 do
     [ "$f" = ".git" ] && continue
@@ -8,4 +5,12 @@ do
     ln -snfv "$DOTPATH/$f" "$HOME"/"$f"
 done
 
+# vscode関連
+VSPATH="${HOME}/Library/Application Support/Code/User"
+
+for s in `ls vscode/snippets/`
+do
+
+  ln -snfv "$DOTPATH/vscode/snippets/$s" "$VSPATH"/snippets/"$s"
+done
 ln -snfv "$DOTPATH/vscode/settings.json" "$VSPATH"/"settings.json"
